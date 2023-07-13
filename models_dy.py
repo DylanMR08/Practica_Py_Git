29221ab4bf3a7d7ef148f9edd5067c0aa4a96dc8
DylanMR08 <dylanmejiaruiz8@gmail.com>
2023-07-12 20:31:44 -0600

    Relacion de tablas creada

--- models_dy.py
+++ models_dy.py
@@ -1,14 +1,17 @@
 from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database import Base
+from typing import List
 
-class User(Base):
-    __tablename__ = "users"
+class Persona(Base):
+    __tablename__ = "persona"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(30), unique=True)
     lastname: Mapped[str] = mapped_column(String(50))
     email: Mapped[str] = mapped_column(String(50), unique=True)
+
+    gatitos: Mapped[List["Gatito"]] = relationship(back_populates="owner")
 
     def __init__(self, **kwargs):
         self.id = kwargs.get("id")
