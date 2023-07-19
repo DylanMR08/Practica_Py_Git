6f3dcf8ebb16fc036dc70a6b7a9a432b9021b621
DylanMR08 <dylanmejiaruiz8@gmail.com>
2023-07-18 18:10:06 -0600

    Routes and Services

---
+++ routes/PersonaRoute.py
@@ -0,0 +1,35 @@
+from typing import Annotated, List
+from fastapi import Depends, APIRouter
+from sqlalchemy.orm import Session
+from database.database import get_db
+from schemas.PersonaSchema import PersonaValidator
+from services.PersonaService import PersonaService
+
+DBSession = Annotated[Session, Depends(get_db)]
+
+personRoute = APIRouter(prefix="/person", tags=["Personas"])
+
+@personRoute.get("/")
+async def index(dbConn: DBSession) -> List[PersonaValidator]:
+    response = PersonaService.get_all(db=dbConn)
+    return response
+
+@personRoute.get("/{id}")
+async def getById(id: int, dbConn: DBSession) -> PersonaValidator:
+    response = PersonaService.get_by(id=id, db=dbConn)
+    return response
+
+@personRoute.delete("/{id}")
+async def getById(id: int, dbConn: DBSession):
+    response = PersonaService.delete(id=id, db=dbConn)
+    return "Eliminado correctamente" if response is True else "Error al eliminar"
+
+@personRoute.post("/")
+async def create(persona: PersonaValidator, dbConn: DBSession):
+    response = PersonaService.create(person=persona, db=dbConn)
+    return "Agregado correctamente" if response is True else "Error al agregar"
+
+@personRoute.put("/")
+async def update(persona: PersonaValidator, dbConn: DBSession):
+    response = PersonaService.update(person=persona, db=dbConn)
+    return "Modificado correctamente" if response is True else "Error al modificar"
--- services/PersonaService.py
+++ services/PersonaService.py
@@ -1,5 +1,6 @@
 from models.models import Persona
 from sqlalchemy.orm import Session
+from schemas.PersonaSchema import PersonaValidator
 from typing import List
 
 class PersonaService:
@@ -8,3 +9,47 @@ class PersonaService:
     def get_all(self, db: Session) -> List[Persona]:
         values = db.query(Persona).all()
         return [person for person in values]
+
+    @classmethod
+    def get_by(self, id: int, db: Session) -> Persona:
+        value = db.query(Persona).get(id)
+        return value
+
+    @classmethod
+    def delete(self, id: int, db: Session) -> bool:
+        try:
+            value = db.query(Persona).get(id)
+            if value is not None:
+                db.delete(value)
+                db.commit()
+                return True
+        except Exception as e:
+            db.rollback()
+            print(e)
+        return False
+
+    @classmethod
+    def create(self, person: PersonaValidator, db: Session) -> bool:
+        try:
+            persona = Persona(**person.model_dump())
+            db.add(persona)
+            db.commit()
+            return True
+        except Exception as e:
+            db.rollback()
+            print(e)
+        return False
+    
+    @classmethod
+    def update(self, person: PersonaValidator, db: Session) -> bool:
+        try:
+            persona = db.query(Persona).get(person.id)
+            persona.name = person.name
+            persona.lastname = person.lastname
+            db.commit()
+            db.refresh(persona)
+            return True
+        except Exception as e:
+            db.rollback()
+            print(e)
+        return False
